eef7d8b1cdd9114a600f147ff6cbd133f597513f
John Ochs <john.w.ochs0@gmail.com>
2019-08-04 08:49:16 -0500

    added starting js and edited css to fit

--- dump.js
+++ dump.js
@@ -1,1 +1,49 @@
+window.onload = function () {
+    $(".put").on("click", start);
+    buttons()
+    return
+};
 
+var gifSelect = ["adventure time", "guinea pigs", "area 51"]
+
+$(".u").on("click", function() {
+    console.log("?")
+    var getin = $(".pull").attr("data-title");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      getin + "&api_key=2seXEbjGc8qFULT62HakfQYCv9rlLO2Y&limit=10";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function (response) {
+        var results = response.data;
+        for (var i = 0; i < results.length; i++) {
+          var gifDiv = $("<div>");
+          var rating = results[i].rating;
+          var p = $("<p>").text("Rating: " + rating);
+          var exact = $("<img>");
+          exact.attr("src", results[i].images.fixed_height.url);
+          gifDiv.prepend(p);
+          gifDiv.prepend(exact);
+          $(".ben").append(gifDiv);
+        }
+    });
+});
+
+function buttons() {
+    $("#buttons").empty();
+    for (q = 0; q < gifSelect.length; q++) {
+        var w = $("<a href='#'>");
+        w.addClass("submit pull");
+        w.attr("data-title", gifSelect[q]);
+        w.text(gifSelect[q]);
+        $("#buttons").append(w);
+    }
+}
+
+function start() {
+    var gif = $("#search").val().trim();
+    gifSelect.push(gif)
+    buttons();
+};
