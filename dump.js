9794b83e9c863e84a27ef20364dd1c03b2fc0f02
John Ochs <john.w.ochs0@gmail.com>
2019-08-04 12:24:36 -0500

    near completion, need only code to pause gifs. only js file modified

--- dump.js
+++ dump.js
@@ -4,31 +4,30 @@ window.onload = function () {
     return
 };
 
-var gifSelect = ["adventure time", "guinea pigs", "area 51"]
+var gifSelect = ["adventure time", "guinea pigs", "area 51", "tesla"]
 
-$(".u").on("click", function() {
-    console.log("?")
-    var getin = $(".pull").attr("data-title");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      getin + "&api_key=2seXEbjGc8qFULT62HakfQYCv9rlLO2Y&limit=10";
+$(document).on("click", ".pull", function () {
+    var getin = $(this).attr("data-title");
+    var QURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        getin + "&api_key=2seXEbjGc8qFULT62HakfQYCv9rlLO2Y&limit=10";
 
     $.ajax({
-      url: queryURL,
-      method: "GET"
+        url: QURL,
+        method: "GET"
     })
-      .then(function (response) {
-        var results = response.data;
-        for (var i = 0; i < results.length; i++) {
-          var gifDiv = $("<div>");
-          var rating = results[i].rating;
-          var p = $("<p>").text("Rating: " + rating);
-          var exact = $("<img>");
-          exact.attr("src", results[i].images.fixed_height.url);
-          gifDiv.prepend(p);
-          gifDiv.prepend(exact);
-          $(".ben").append(gifDiv);
-        }
-    });
+        .then(function (response) {
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div class = ''>");
+                var rating = results[i].rating;
+                var p = $("<p>").text("Rating: " + rating);
+                var exact = $("<img>");
+                exact.attr("src", results[i].images.fixed_height.url);
+                gifDiv.prepend(p);
+                gifDiv.prepend(exact);
+                $(".ben").after(gifDiv);
+            }
+        });
 });
 
 function buttons() {
